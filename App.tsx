e4d814c160e2cbfa005d8aa0e6170f61aa49a907
Eduardo_Mourao <edumouraoaragao@gmail.com>
2026-05-22 14:18:59 -0300

    feat: inicializa estrutura do projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#07111f" />
+      <Text style={styles.title}>ProxAlert</Text>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#07111f',
   },
-});
+  title: {
+    color: '#ffffff',
+    fontSize: 28,
+    fontWeight: '800',
+  },
+})
